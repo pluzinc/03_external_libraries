2782c8993ad9ffa272c8f9ac2d6f0e83ce606649
pluzinc <132268195+pluzinc@users.noreply.github.com>
2023-10-15 17:57:56 -0400

    add orbit control so the model is interactive

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+let object;
+//OrbitControls allow the camera to move around the scene
+let controls;
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -8,24 +13,46 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//Keep track of the mouse position
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+// make the cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// scene.add(cube);
+// add the light
+const light = new THREE.AmbientLight(0x404040, 30); // soft white light
+// light.castShadow = true;
+scene.add(light);
+
 
 camera.position.z = 5;
-
+// load my model
 const loader = new GLTFLoader();
 
-loader.load('export_yellow_candy.glb', function (gltf) {
+loader.load('export_yellow_candy.glb',
+    function (gltf) {
+        object = gltf.scene
 
-    scene.add(gltf.scene);
+        scene.add(object);
 
-}, undefined, function (error) {
+    }, undefined, function (error) {
 
-    console.error(error);
+        console.error(error);
 
+    });
+//Add a listener to the window, so we can resize the window and the camera
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+//This adds controls to the camera, so we can rotate / zoom it with the mouse
+
+controls = new OrbitControls(camera, renderer.domElement);
+
 
 function animate() {
     requestAnimationFrame(animate);
